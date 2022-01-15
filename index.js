8e31acd5e91d124e7234650b493014db4c8157fe
Ptruckstar <shamshab00@gmail.com>
2022-01-15 20:19:27 +0800

    Tuhan kumohon untuk kali ini gak ngulang lagi 🙏🏼😭😭

--- index.js
+++ index.js
@@ -56,7 +56,4 @@ async function main(req, res) {
 const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => {
   console.log("runing");
-  const safari = require("safari");
-    const url = `http://127.0.0.1:${PORT}`;
-    safari.open( url + "/subtitle?url=https://subscene.com/subtitles/indonesian-text/k8iFzGQgnyQtJ5jvTRJnTrmh8NL5Nppa2w6PSTnuI9CnPndMx1IgUnI-WAcWVAyP1YFF8xWPaW_VGOaDapfdW9UmdJ40bXAtM3-TUgJaLjqPDWSAwyDn68-kPPohL5ge0");
 });
